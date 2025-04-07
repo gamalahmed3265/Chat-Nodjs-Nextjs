440662fcb56b01a329cffec8d93d610596d60129
= <=>
2025-04-07 16:29:25 +0200

    add signin controllers

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -6,14 +6,17 @@ import {
   signup,
   updateProfile,
 } from "../controllers/auth.controller.js";
-import { authSignUpValidation } from "../validation/auth.js";
+import {
+  authSignInValidation,
+  authSignUpValidation,
+} from "../validation/auth.js";
 import { validationMiddelWare } from "../middleware/auth.middleware.js";
 // import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", validationMiddelWare(authSignUpValidation), signup);
-router.post("/login", login);
+router.post("/login", validationMiddelWare(authSignInValidation), login);
 router.post("/logout", logout);
 
 // router.put("/update-profile", protectRoute, updateProfile);
--- backend/src/validation/auth.js
+++ backend/src/validation/auth.js
@@ -6,3 +6,7 @@ export const authSignUpValidation = Joi.object({
   password: Joi.string().min(6).required(),
   profilePic: Joi.string().optional,
 });
+export const authSignInValidation = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
